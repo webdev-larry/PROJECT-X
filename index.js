3478dbfaa1927e60baa18c24a0e873904582390b
larry <adeshinaquamdeen@gmail.com>
2024-02-25 00:49:14 +0000

    scroll

--- index.js
+++ index.js
@@ -58,3 +58,50 @@ function search() {
       }
     }
   }
+
+
+
+  //horizontal scrolling
+  document.addEventListener("DOMContentLoaded", function () {
+    const scrollImages = document.querySelector("#scroll-images");
+    const scrollLength = scrollImages.scrollWidth - scrollImages.clientWidth;
+    const leftButton = document.querySelector("#left");
+    const rightButton = document.querySelector("#right");
+
+    function checkScroll() {
+      const currentScroll = scrollImages.scrollLeft;
+      if (currentScroll === 0) {
+        leftButton.setAttribute("disabled", "true");
+        rightButton.removeAttribute("disabled");
+      } else if (currentScroll === scrollLength) {
+        rightButton.setAttribute("disabled", "true");
+        leftButton.removeAttribute("disabled");
+      } else {
+        leftButton.removeAttribute("disabled");
+        rightButton.removeAttribute("disabled");
+      }
+    }
+
+    scrollImages.addEventListener("scroll", checkScroll);
+    window.addEventListener("resize", checkScroll);
+    checkScroll();
+
+    function leftScroll() {
+      scrollImages.scrollBy({
+        left: -200,
+        behavior: "smooth"
+      });
+    }
+
+    function rightScroll() {
+      scrollImages.scrollBy({
+        left: 200,
+        behavior: "smooth"
+      });
+    }
+
+    leftButton.addEventListener("click", leftScroll);
+    rightButton.addEventListener("click", rightScroll);
+  });
+
+
