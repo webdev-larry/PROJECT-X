1f2f6e8b344473d036a7986f5f5faf73bf6c8ead
larry <adeshinaquamdeen@gmail.com>
2024-03-16 16:29:24 +0000

    js

--- index.js
+++ index.js
@@ -24,15 +24,7 @@ function toggle() {
 
 //body search
 
-function search() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('myInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName('li');
-  
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
+
    
 
 
