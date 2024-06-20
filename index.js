f00b6b8014b3a21f128be815df0ee2bc1d3f16c3
larry <adeshinaquamdeen@gmail.com>
2024-06-20 13:00:53 +0100

    boostrap

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-// var toggleBtn =document.querySelector(".toggle-btn")
-// var toggleBtnIcon =document.querySelector(".toggle-btn i")
-// var dropDownMenu =document.querySelector(".dropdown-link")
+var toggleBtn =document.querySelector(".toggle-btn")
+var toggleBtnIcon =document.querySelector(".toggle-btn i")
+var dropDownMenu =document.querySelector(".dropdown-link")
 
-// toggleBtn.onclick=function () {
-// dropDownMenu.classList.toggle('open')
-// var isOpen = dropDownMenu.classList.contains('open')
-// toggleBtnIcon.classList = isOpen
-// ? "fa-solid fa-bars"
-// :  "fa-solid fa-xmark"
+toggleBtn.onclick=function () {
+dropDownMenu.classList.toggle('open')
+var isOpen = dropDownMenu.classList.contains('open')
+toggleBtnIcon.classList = isOpen
+? "fa-solid fa-bars"
+:  "fa-solid fa-xmark"
 
-// }
-// //make it visible and dissappear
-// function toggle() {
-//     var drop = document.getElementById("dropdown");
-//     if (drop.style.display === "none") {
-//       drop.style.display = "block";
-//     } else {
-//       drop.style.display = "none";
-//     }
-//   }
+}
+//make it visible and dissappear
+function toggle() {
+    var drop = document.getElementById("dropdown");
+    if (drop.style.display === "none") {
+      drop.style.display = "block";
+    } else {
+      drop.style.display = "none";
+    }
+  }
 
    
 
